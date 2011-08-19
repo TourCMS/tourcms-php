0f0ca2c28e3000203d30284c9f570950450aedad
Paul Slugocki <paulslugocki@gmail.com>
2011-08-19 10:19:15 +0100

    Tweaked basic enquiry example to match the example on the API method page

--- examples/enquiry/index.php
+++ examples/enquiry/index.php
@@ -6,17 +6,17 @@
 	include '../config.php';
 	
 	// Partners override the Channel ID here if you want
-	//$channel_id = 0;
+	// $channel_id = 0;
 	
 	// Create a new Instance of the TourCMS API class
 	$tc = new TourCMS($marketplace_account_id, $api_private_key, $result_type);
 	
 	$enquiry = new SimpleXMLElement('<enquiry />');
 	$enquiry->addChild('title', 'Mr');
-	$enquiry->addChild('firstname', 'Test');
-	$enquiry->addChild('surname', 'McTest');
-	$enquiry->addChild('enquiry_type', 'Brochure request');
-	$enquiry->addChild('enquiry_detail', 'Testing testing');
+	$enquiry->addChild('firstname', 'Joe');
+	$enquiry->addChild('surname', 'Bloggs');
+	$enquiry->addChild('enquiry_type', 'General Enquiry');
+	$enquiry->addChild('enquiry_detail', 'Customer comments go here');
 	//$enquiry->addChild('enquiry_followup_date', '2012-04-01');
 	
 	$result = $tc->create_enquiry($enquiry, $channel_id);
