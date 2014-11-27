38d83610495c34032fb76147d80636ca9fa0daad
paulslugocki <paulslugocki@gmail.com>
2014-11-27 17:20:01 +0000

    Added map example to examples index list

--- examples/index.php
+++ examples/index.php
@@ -11,33 +11,39 @@
 		<h4>For all account types</h4>
 		<ul>
 			<li>
-				<a href="anyone/api_rate_limit_status/">Check current API rate limit status</a> 
+				<a href="anyone/api_rate_limit_status/">Check current API rate limit status</a>
 				<span class="ancillary">
 					<a href="http://www.tourcms.com/support/api/mp/rate_limit_status.php" target="_blank">API rate limit status</a>
 				</span>
 			</li>
 			<li>
-				<a href="anyone/search_tours/">Display a list of Tours, link through to supplier site for full details</a> 
+				<a href="anyone/search_tours/">Display a list of Tours, link through to supplier site for full details</a>
 				<span class="ancillary">
 					<a href="http://www.tourcms.com/support/api/mp/tour_search.php" target="_blank">Search Tours</a>
 				</span>
 			</li>
 			<li>
-				<a href="anyone/search_and_show_tours/">Display a list of Tours, stay on site when viewing tour details</a> 
+				<a href="anyone/search_and_show_tours/">Display a list of Tours, stay on site when viewing tour details</a>
 				<span class="ancillary">
 					<a href="http://www.tourcms.com/support/api/mp/tour_search.php" target="_blank">Search Tours</a>
 						<a href="http://www.tourcms.com/support/api/mp/tour_search.php" target="_blank">Show Tour</a>
 				</span>
 			</li>
+			<li>
+				<a href="anyone/show_tour_map/">Generate a Google Map of a Tour start location</a> 
+				<span class="ancillary">
+						<a href="http://www.tourcms.com/support/api/mp/tour_search.php" target="_blank">Show Tour</a>
+				</span>
+			</li>
 			<!--li>
-				<a href="anyone/search_tours_paged/">Display a list of Tours/Hotels with pagination</a> 
+				<a href="anyone/search_tours_paged/">Display a list of Tours/Hotels with pagination</a>
 				<span class="ancillary">
 					<a href="http://www.tourcms.com/support/api/mp/search_tours.php">Search Tours/Hotels</a>
 				</span>
 			</li-->
 		</ul>
 		<p>More examples in future versions of the wrapper, for now check out the <a href="http://www.tourcms.com/support/api/mp/examples.php" target="_blank">examples page on our website</a> and don't forget each of the methods in <a href="http://www.tourcms.com/support/api/mp/" target="_blank">our documentation</a> contains sample code.</p>
-		
+
 		<!--h4>For Operator/Supplier accounts only</h4>
 		<h4>For Partner/Affiliate accounts only</h4-->
 		<h2>Problems?</h2>
