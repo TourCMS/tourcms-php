4e7c2a2db6ecb23c2a3bbac3c54911190f0a21ad
Cornelius Carstens <c.carstens@bvb.net>
2020-03-30 19:24:36 -0700

    added phpfastcache as dev dependency for unit testing

--- tests/TourCMSCachingTest.php
+++ tests/TourCMSCachingTest.php
@@ -4,12 +4,17 @@
 namespace TourCMS\Utils\tests;
 
 
+use Phpfastcache\Helper\Psr16Adapter;
 use PHPUnit\Framework\TestCase;
+use TourCMS\Utils\TourCMS;
 
 class TourCMSCachingTest extends TestCase
 {
     /** @test */
-    public function it_is_a_first_test(){
-        $this->assertTrue(true);
+    public function it_can_receive_a_psr16_instance_as_constructor_argument(){
+        $cache = new Psr16Adapter("Files");
+        $tourcms = new TourCMS(0, "key", "simplexml", 0, $cache);
+
+        $this->assertInstanceOf(TourCMS::class, $tourcms);
     }
 }
