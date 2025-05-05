c64bbee4e58e81cc1a66f50392af9ce422a5e942
Belen Ruiz <belen.ruiz@palisis.com>
2025-05-05 16:37:04 +0200

    Created endpoints for customer verification and create

--- src/TourCMS.php
+++ src/TourCMS.php
@@ -691,6 +691,23 @@ class TourCMS {
 		return($this->request('/c/customers/login_search.xml?customer_username='.$customer.'&customer_password='.$password, $channel));
 	}
 
+	public function customer_verification($method,$username,$redirect_url,$token_placement = ''){
+		if ($token_placement !== ''){
+			return($this->request('/c/customer/verification.xml?verification_methods.method='.$method.'&username='.$username.'&redirect_url='.$redirect_url.'&token_placement='.$token_placement));
+		} else {
+			return($this->request('/c/customer/verification.xml?verification_methods.method='.$method.'&username='.$username.'&redirect_url='.$redirect_url));
+		}
+		
+	}
+
+	public function create_customer($email,$firstname,$surname,$username = ''){
+		if ($firstname !== ''){
+			return($this->request('/c/customer/create.xml?email='.$email.'&username='.$username.'&firstname='.$firstname.'&surname='.$surname));
+		} else {
+			return($this->request('/c/customer/create.xml?email='.$email.'&username='.$username.'&surname='.$surname));
+		}
+	}
+
 	# Agents
 	public function search_agents($params, $channel)
 	{
